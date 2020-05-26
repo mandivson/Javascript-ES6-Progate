b3d30b96b2865a41513378cf55530a66997a7bc9
Manish Ranjan <53346067+mandivson@users.noreply.github.com>
2020-05-26 22:41:10 +0530

    Add files via upload

---
+++ Npm packages and multiple files/page4/script.js
@@ -0,0 +1,6 @@
+// Rewrite the below and import the constants dog1 and dog2
+import {dog1,dog2} from "./dogData";
+
+// Copy from the instructions and rewrite it so that the constants dog1 and dog2 are printed
+dog1.info();
+dog2.info();
